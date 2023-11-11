8510c7fe7a2463d7842f7cc322ba03ea6a9c5172
kamal <kamal@netxperts.com>
2023-11-11 05:19:20 +0000

    2nd Statge Modified

--- slwaters.java
+++ slwaters.java
@@ -3,3 +3,8 @@ class Animal {
     System.out.println("The animal makes a sound");
   }
 }
+class Pig extends Animal {
+  public void animalSound() {
+    System.out.println("The pig says: wee wee");
+  }
+}
